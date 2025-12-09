a0aee54deb532969276cd88f117cdd044395f67f
divya20242006 <diyud042@gmail.com>
2025-12-08 21:42:12 -1000

    Fix formatting and add new lines in variable.js
    Signed-off-by: divya20242006 <diyud042@gmail.com>

--- variable.js
+++ variable.js
@@ -3,6 +3,7 @@ const COLOR_GREEN = "#0F0";
 const COLOR_BLUE = "#00F";
 const COLOR_ORANGE = "#FF7F00";
 
-// ...when we need to pick a color
+
 let color = COLOR_ORANGE;
-console.log(color); // #FF7F00
+
+console.log(color); 
